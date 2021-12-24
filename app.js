9284cadd75b88128430f6d427f96a3c9bff42de9
Somnath Tiwari <somnnathtiwari96@gmail.com>
2021-12-24 19:20:57 +0530

    changed Port

--- app.js
+++ app.js
@@ -166,7 +166,9 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
+const PORT = procces.env.PORT || 3000
+
 app.listen(3000, () => {
-    console.log('Serving on port 3000')
+    console.log(`running on ${PORT}`)
 })
 
