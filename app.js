f90b36f7900ea9b21f2a668338fabaef14dd2987
Somnath Tiwari <somnnathtiwari96@gmail.com>
2021-12-24 19:33:26 +0530

    done

--- app.js
+++ app.js
@@ -166,7 +166,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-const PORT = procces.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(3000, () => {
     console.log(`running on ${PORT}`)
